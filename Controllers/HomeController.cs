e7139202e102f76ce8d64966a88e7ed72b1ef6f1
K.H.U.R.R.A.M <khurram.shahzad@ansio.com>
2021-05-09 11:38:56 +0100

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿namespace problem_detail_sample.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index(string name)
+        {
+            try
+            {
+                if (string.Equals(name, "khurram"))
+                {
+                    return Ok("good response");
+                }
+                else
+                {
+                    throw new Exception("Testing Problem Detail");
+                }
+            }
+            catch (Exception ex)
+            {
+                var problemDetails = new Microsoft.AspNetCore.Mvc.ProblemDetails
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Type = "Home Controller Index ",
+                    Title = ex.Message,
+                    Detail = ex.StackTrace,
+                    Instance = HttpContext.Request.Path
+                };
+                return BadRequest(problemDetails);
+            }
+        }
+
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Introduction
+
+This is a sample code how to use Problem Details in dotnet core application. 
+More information can be found on https://datatracker.ietf.org/doc/html/rfc3986
+
+Whenever you are building http apis then we see inconsistencies in returning error response back to the 
+client. With Problem Details response we come up with a standard approach.
+ 
